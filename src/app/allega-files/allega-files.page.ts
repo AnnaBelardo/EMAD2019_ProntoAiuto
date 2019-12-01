b80fa7139c56785c9a1a02810ef23a9d9b72e076
ANNA BELARDO <anna.belardo94@gmail.com>
2019-12-02 00:21:37 +0100

    aggiunta funzionalità foto dalla pagina "allega informazioni"

--- src/app/allega-files/allega-files.page.ts
+++ src/app/allega-files/allega-files.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-allega-files',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./allega-files.page.scss'],
 })
 export class AllegaFilesPage implements OnInit {
+  capturedSnapURL: string;
 
-  constructor() { }
+  cameraOptions: CameraOptions = {
+    quality: 20,
+    destinationType: this.camera.DestinationType.DATA_URL,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE
+  } ;
+  constructor(private camera: Camera) { }
 
   ngOnInit() {
   }
-
+  takePicture() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    };
+    this.camera.getPicture(this.cameraOptions).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+      const base64Image = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      // Handle error
+      console.log(err);
+    });
+  }
 }
+
+
