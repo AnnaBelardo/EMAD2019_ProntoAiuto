38b5002a2f9aa339d098dc997166932da06a30de
raffaeledeluca <raffaeledeluca92@gmail.com>
2019-12-30 11:48:36 +0100

    rimosso .idea

--- Cittadino/src/app/allega-files/allega-files.page.ts
+++ Cittadino/src/app/allega-files/allega-files.page.ts
@@ -14,6 +14,7 @@ import {ActivatedRoute, Router} from '@angular/router';
 // import {HTTP} from '@ionic-native/http/ngx';
 
 
+
 @Component({
   selector: 'app-allega-files',
   templateUrl: './allega-files.page.html',
@@ -264,6 +265,7 @@ export class AllegaFilesPage implements OnInit {
 //      asd: 'asd'
 //    });
 //  }
+
 
 
 }
