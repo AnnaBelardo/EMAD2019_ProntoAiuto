d1e0ff29fd9ba4b24c8e868a17381540154654a7
Vincenzo-DR <57796878+Vincenzo-DR@users.noreply.github.com>
2020-01-26 12:32:08 +0100

    Eliminazione vecchia API
    Eliminazione vecchia API per la comunicazione con il back end.

--- rest-api/src/backend/Dtos/CreateVetturaDto.ts
+++
@@ -1,7 +0,0 @@
-export class CreateVetturaDto {
-    public identificativo: string;
-    public tipologia: string;
-    public stato: string;
-    public imei: string;
-    public playerId: string;
-}
