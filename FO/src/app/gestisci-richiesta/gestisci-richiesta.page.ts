9e2c15fb1b61351c054b2d97c9b366627bf10c36
ANNA BELARDO <anna.belardo94@gmail.com>
2020-01-26 18:31:52 +0100

    add alert richiesta linea verde

--- FO/src/app/gestisci-richiesta/gestisci-richiesta.page.ts
+++ FO/src/app/gestisci-richiesta/gestisci-richiesta.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {LaunchNavigator, LaunchNavigatorOptions} from '@ionic-native/launch-navigator/ngx';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-gestisci-richiesta',
@@ -8,7 +9,8 @@ import {LaunchNavigator, LaunchNavigatorOptions} from '@ionic-native/launch-navi
 })
 export class GestisciRichiestaPage implements OnInit {
 
-  constructor(private launchNavigator: LaunchNavigator) { }
+  constructor(private launchNavigator: LaunchNavigator,
+              public alertController: AlertController) { }
   options: LaunchNavigatorOptions = {
     app: this.launchNavigator.APP.GOOGLE_MAPS
   };
@@ -21,6 +23,16 @@ export class GestisciRichiestaPage implements OnInit {
             success => console.log('launched navigator'),
             error => console.log('launched navigator error')
         );
+  }
+
+  async showalert() {
+    const alert = await this.alertController.create({
+      header: 'Richiesta effettuata',
+      message: 'La richiesta di linea verde è stata inoltrata con successo.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
   }
 
 }
