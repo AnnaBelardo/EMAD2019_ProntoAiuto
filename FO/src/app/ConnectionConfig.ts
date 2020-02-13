14a7f322341d3b3e454493e9cf976850225aa265
raffaeledeluca <raffaeledeluca92@gmail.com>
2020-02-13 19:44:23 +0100

    fix minor bug richesta->richiesta

--- FO/src/app/ConnectionConfig.ts
+++ FO/src/app/ConnectionConfig.ts
@@ -1,6 +1,6 @@
 export class ConnectionConfig {
-    static ip = '192.168.1.253';
-    static port = '8080';
+    static ip = '192.168.43.119';
+    static port = '8100';
     static getBaseUrl() {
         return 'http://' + this.ip + ':' + this.port;
     }
