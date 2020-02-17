c1647ebc0461bff143de3c9feca26126e0dda683
ANNA BELARDO <anna.belardo94@gmail.com>
2020-02-17 17:46:16 +0100

    add regoa css bottone alert

--- Cittadino/src/app/ConnectionConfig.ts
+++ Cittadino/src/app/ConnectionConfig.ts
@@ -1,6 +1,6 @@
 export class ConnectionConfig {
-    static ip = '192.168.43.119';
-    static port = '8100';
+    static ip = '192.168.43.244';
+    static port = '8080';
     static getBaseUrl() {
         return 'http://' + this.ip + ':' + this.port;
     }
